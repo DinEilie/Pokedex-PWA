34ff2b63a87273cf7be53acd1cbeae4257b29a25
Din Eilie (דין אילי) <98718983+DinEilie@users.noreply.github.com>
2024-03-24 03:38:20 +0200

    Update - 24.03.2024
    ( - ) Finishing setting up Vite PWA plugin.

--- vite-pokedex/vite.config.ts
+++ vite-pokedex/vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pokedex - By Din Eilie',
+        short_name: 'Pokedex',
+        description: 'An awesome pokedex application',
+        theme_color: '#ffffff',
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
